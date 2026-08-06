1ede4aa5ff114640819b56aca4d0642780209963
xinxinqingnian <3890241616@qq.com>
2026-08-06 14:39:24 +0800

    Update bookData.js

--- bookData.js
+++ bookData.js
@@ -1,8 +1,8 @@
-const bookData = [
-    { 
-        name: "《共产党宣言》", 
-        author: "马克思 / 恩格斯", 
-        tag: "纲领性文献", 
-        url: "books/《共产党宣言》全文.Pdf" 
-    }
+window.bookData = [
+  {
+    name: "《共产党宣言》",
+    author: "马克思 / 恩格斯",
+    tag: "纲领性文献",
+    url: "books/《共产党宣言》全文.Pdf"
+  }
 ];
